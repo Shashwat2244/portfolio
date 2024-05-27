feb9cf492b81e2eb8365daad177f6e6aba405f13
ShashwatShrivastava <sshrivastava4@baskethunt.in>
2024-05-27 14:11:37 +0530

    Updated animation

--- project_metaverse-main/components/InsightCard.jsx
+++ project_metaverse-main/components/InsightCard.jsx
@@ -6,7 +6,7 @@ import { fadeIn } from "../utils/motion";
 
 const InsightCard = ({ imgUrl, title, subtitle, index, link }) => (
   <motion.div
-    variants={fadeIn("up", "spring", index * 0.5, 1)}
+    // variants={fadeIn("up", "spring", index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
   >
     <img
