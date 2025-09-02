2c485115bef1d445600a8011fc4b510b42eb7357
Shashwat Shrivastava <88186360+Shashwat2244@users.noreply.github.com>
2025-09-02 22:11:01 +0530

    Update index.js

--- project_metaverse-main/constants/index.js
+++ project_metaverse-main/constants/index.js
@@ -45,11 +45,11 @@ export const startingFeatures = [
 
 export const experienceFeatures = [
   {
-    id: 'exp-2',
+    id: 'exp-1',
     imgUrl: '/bg2.jpg',
     title: 'Associate Software Consultant - Mindsprint (01/2025 - Present)',
   },{
-    id: 'exp-1',
+    id: 'exp-2',
     link: 'https://drive.google.com/drive/folders/1wIk3bxch2mcXD6V7HzD5PWH_FxYnw5fG?usp=sharing',
     imgUrl: '/bg1.jpg',
     title: 'Web Development Intern - Baskethunt Pvt. Ltd. (08/2023 - 10/2023)',
